5c39cbdf8620c0aa835d25f6f4ed4ab2e94aa0a1
Dari Alves <dari.alves.pt@gmail.com>
2024-03-14 23:30:56 +0000

    control flow

--- fundamentos-do-java/HelloWorld.java
+++ fundamentos-do-java/HelloWorld.java
@@ -7,5 +7,31 @@ public class HelloWorld {
         long numLong = 87835254454L;
         String stringType = "Java";
         boolean booleanType = true;
+
+        // if - else 
+        if(numInt == 11) {
+            // sysout
+            System.out.println("IF - 10");
+        } else if (numInt == 12) {
+            System.out.println("IF - 12");
+        } else {
+            System.out.println("ELSE");
+        }
+
+        // While
+        int initialValue = 0;
+        while (initialValue < 3) {
+            System.out.println("The initial value is less than 3");
+            System.out.println(initialValue);
+            initialValue++;
+        }
+        System.out.println("Outside the 'while loop'");
+
+        // For
+        System.out.println("Initial FOR");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Value: " + i);
+        }
+        System.out.println("Final FOR");
     }
 }
