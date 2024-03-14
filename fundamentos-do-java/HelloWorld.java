7f3d28ac1371cf6cee3cc2ad944d7776eca83dcc
Dari Alves <dari.alves.pt@gmail.com>
2024-03-14 23:14:52 +0000

    data types

---
+++ fundamentos-do-java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Data Types
+        int numInt = 10;
+        double numDouble = 3.14;
+        float numFloat = 3.14f;
+        long numLong = 87835254454L;
+        String stringType = "Java";
+        boolean booleanType = true;
+    }
+}
